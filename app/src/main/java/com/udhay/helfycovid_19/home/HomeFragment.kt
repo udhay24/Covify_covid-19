8be6cdb78142f540d8c534ec49b849b70b30bf84
udhay24 <udhay2498@gmail.com>
2020-03-24 00:52:40 +0530

    added maps to the layout

--- app/src/main/java/com/udhay/helfycovid_19/home/HomeFragment.kt
+++ app/src/main/java/com/udhay/helfycovid_19/home/HomeFragment.kt
@@ -1,10 +1,16 @@
 package com.udhay.helfycovid_19.home
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.lifecycle.Observer
 
 import com.udhay.helfycovid_19.R
@@ -43,6 +49,7 @@ class HomeFragment : Fragment() {
                 is Resource.LoadingResponse -> Unit //TODO() show loading message
             }
         })
+        displayMap()
     }
 
     private fun updateWorldInfo(worldModel: WorldModel) {
@@ -59,6 +66,23 @@ class HomeFragment : Fragment() {
         val worstStates = stateModel
             .sortedBy { it.confirmed_cases }
             .subList(0, 5)
-
+        states_recycler_view.adapter = StatesRecyclerAdapter(worstStates)
     }
+
+    private fun displayMap() {
+        val mapUrl = "https://who.maps.arcgis.com/apps/opsdashboard/index.html#/c88e37cfc43b4ed3baf977d77e4a0667"
+        affected_web_view.settings.javaScriptEnabled = true // enable javascript
+        affected_web_view.webViewClient = object: WebViewClient() {
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+                super.onReceivedError(view, request, error)
+                Toast.makeText(this@HomeFragment.requireContext(), error?.description, Toast.LENGTH_SHORT).show()
+            }
+        }
+        affected_web_view.loadUrl(mapUrl)
+    }
+
 }
