12513cae8b484ecfeaed40e27a5a46f5b02a4b82
udhay24 <udhay2498@gmail.com>
2020-03-25 13:30:33 +0530

    changed twitter layout

--- app/src/main/java/com/udhay/helfycovid_19/home/TwitterRecyclerAdapter.kt
+++ app/src/main/java/com/udhay/helfycovid_19/home/TwitterRecyclerAdapter.kt
@@ -1,12 +1,16 @@
 package com.udhay.helfycovid_19.home
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import com.udhay.helfycovid_19.R
 import kotlinx.android.synthetic.main.twitter_view_holder_layout.view.*
 import twitter4j.Status
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TwitterRecyclerAdapter(
     private val tweets: List<Status>
@@ -24,8 +28,17 @@ class TwitterRecyclerAdapter(
     }
 
     inner class TwitterViewModel(private val view: View): RecyclerView.ViewHolder(view) {
+        private val dateFormat = SimpleDateFormat("HH:mm dd-MM", Locale.ENGLISH)
         fun bindTweet(tweet: Status) {
-            view.tweet_body.text = tweet.text
+            view.profile_id.text = tweet.user.name
+            view.tweet_time_text_view.text = dateFormat.format(tweet.createdAt)
+            view.tweet_body_text_view.text = tweet.text
+            Log.v("Image", tweet.user.originalProfileImageURL)
+            Picasso.get()
+                .load(tweet.user.originalProfileImageURL.trim())
+                .fit()
+                .centerCrop()
+                .into(view.profile_icon)
         }
     }
 }
