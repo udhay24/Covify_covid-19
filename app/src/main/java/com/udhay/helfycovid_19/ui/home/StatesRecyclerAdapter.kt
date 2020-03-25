09484d8b3be2efb217788e124c1a828d1492e6fd
Gaurav <gauravrai568@gmail.com>
2020-03-26 02:21:48 +0530

    Dashboard and tweeter screen changes done

--- app/src/main/java/com/udhay/helfycovid_19/ui/home/StatesRecyclerAdapter.kt
+++ app/src/main/java/com/udhay/helfycovid_19/ui/home/StatesRecyclerAdapter.kt
@@ -3,6 +3,7 @@ package com.udhay.helfycovid_19.ui.home
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.udhay.helfycovid_19.R
 import com.udhay.helfycovid_19.data.model.StateModel
@@ -21,12 +22,33 @@ class StatesRecyclerAdapter(
     override fun getItemCount(): Int = statesList.size
 
     override fun onBindViewHolder(holder: StatesViewModel, position: Int) {
-        holder.populateDetails(state = statesList[position])
+        val pos = position
+        holder.populateDetails(state = statesList[position], position = pos)
     }
 
     inner class StatesViewModel(private val view: View): RecyclerView.ViewHolder(view) {
-        fun populateDetails(state: StateModel.StateModelItem) {
+        fun populateDetails(state: StateModel.StateModelItem, position: Int) {
+
             view.let {
+
+                if (position == 0) {
+                    it.constraint_Layout.background = it.context.getDrawable(R.drawable.state_card_gradient)
+                    it.state_title_text_view.setTextColor(it.context.getColor(R.color.white))
+                    it.cases_text_view.setTextColor(it.context.getColor(R.color.white))
+                    it.death_text_view.setTextColor(it.context.getColor(R.color.white))
+                    it.recovered_text_view.setTextColor(it.context.getColor(R.color.white))
+                    it.detail_button.setTextColor(it.context.getColor(R.color.white))
+                    it.imageView4.setColorFilter(it.context.getColor(R.color.pink))
+                    it.imageView5.setColorFilter(it.context.getColor(R.color.pink))
+                    it.imageView6.setColorFilter(it.context.getColor(R.color.pink))
+                    it.view.setBackgroundColor(it.context.getColor(R.color.white))
+                    it.view.alpha = 0.2f
+                    it.view2.setBackgroundColor(it.context.getColor(R.color.white))
+                    it.view2.alpha = 0.1f
+                    it.view3.setBackgroundColor(it.context.getColor(R.color.white))
+                    it.view3.alpha = 0.1f
+                }
+
                 it.state_title_text_view.text = state.state
                 it.cases_text_view.text = state.confirmed_case.toString()
                 it.death_text_view.text = state.death.toString()
