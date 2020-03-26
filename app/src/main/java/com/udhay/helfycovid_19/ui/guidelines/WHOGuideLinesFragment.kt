c2a27b49504faa18cc99d89565872bd8744f611d
Gaurav <gauravrai568@gmail.com>
2020-03-27 02:09:53 +0530

    guidelines done

--- app/src/main/java/com/udhay/helfycovid_19/ui/guidelines/WHOGuideLinesFragment.kt
+++ app/src/main/java/com/udhay/helfycovid_19/ui/guidelines/WHOGuideLinesFragment.kt
@@ -1,24 +1,26 @@
 package com.udhay.helfycovid_19.ui.guidelines
 
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.TextView
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.synnapps.carouselview.ViewListener
 import com.udhay.helfycovid_19.R
 import kotlinx.android.synthetic.main.fragment_who_guide_lines.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class WHOGuideLinesFragment : Fragment() {
 
-    private val guidelinesText = listOf("Wrote water woman of heart it total other. By in entirely securing suitable graceful at families improved. Zealously few furniture repulsive was agreeable consisted difficult. Collected breakfast estimable questions in to favourite it. Known he place worth words it as to. Spoke now noise off smart her ready.",
-    "Arrived compass prepare an on as. Reasonable particular on my it in sympathize. Size now easy eat hand how. Unwilling he departure elsewhere dejection at. Heart large seems may purse means few blind. Exquisite newspaper attending on certainty oh suspicion of. He less do quit evil is. Add matter family active mutual put wishes happen.",
-    "No depending be convinced in unfeeling he. Excellence she unaffected and too sentiments her. Rooms he doors there ye aware in by shall. Education remainder in so cordially. His remainder and own dejection daughters sportsmen. Is easy took he shed to kind.",
-    "Impossible considered invitation him men instrument saw celebrated unpleasant. Put rest and must set kind next many near nay. He exquisite continued explained middleton am. Voice hours young woody has she think equal. Estate moment he at on wonder at season little. Six garden result summer set family esteem nay estate. End admiration mrs unreserved discovered comparison especially invitation",
-    "In post mean shot ye. There out her child sir his lived. Design at uneasy me season of branch on praise esteem. Abilities discourse believing consisted remaining to no. Mistaken no me denoting dashwood as screened. Whence or esteem easily he on. Dissuade husbands at of no if disposal.")
+    var guidelinesText = listOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,7 +33,43 @@ class WHOGuideLinesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        guide_lines_recyler_view.adapter = GuideLinesAdapter(guidelinesText)
+        guidelinesText = listOf(getString(R.string.guideline_1), getString(R.string.guideline_2),
+        getString(R.string.guideline_3), getString(R.string.guideline_4), getString(R.string.guideline_5))
+        guide_lines_recyler_view.setViewListener(viewListener)
+        guide_lines_recyler_view.pageCount = 5
+
+        val swc= "https://www.mohfw.gov.in/coronvavirushelplinenumber.pdf"
+        link1.setOnClickListener {
+            launchWebsite("https://www.mohfw.gov.in/coronvavirushelplinenumber.pdf")
+        }
+        link2.setOnClickListener {
+            launchWebsite("https://www.mohfw.gov.in")
+        }
+        link3.setOnClickListener {
+            launchWebsite("https://www.who.int/emergencies/diseases/novel-coronavirus-2019")
+        }
+        link4.setOnClickListener {
+            launchWebsite("https://coronavirus.thebaselab.com")
+        }
+
     }
+
+    val viewListener = ViewListener { position ->
+        val view = layoutInflater.inflate(R.layout.guidelines_view_holder, null)
+        val text = view.findViewById<TextView>(R.id.guide_line_text_view)
+        text.text = guidelinesText[position]
+        view
+    }
+
+    private fun launchWebsite(url: String) {
+        val builder = CustomTabsIntent.Builder()
+        builder.setToolbarColor(ContextCompat.getColor(requireContext(), R.color.colorPrimary))
+        builder.addDefaultShareMenuItem()
+        builder.setShowTitle(true)
+        builder.setCloseButtonIcon(BitmapFactory.decodeResource(resources, android.R.drawable.ic_menu_close_clear_cancel))
+        builder.setExitAnimations(requireContext(), android.R.anim.fade_in, android.R.anim.fade_out)
+        builder.build().launchUrl(requireContext(), Uri.parse(url))
+    }
+
 }
 
