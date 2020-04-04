69260b67e9ef846bf103bd05dbbdc1a66bbb2f20
udhay24 <udhay2498@gmail.com>
2020-04-04 16:46:34 +0530

    added in-app update feature from firebase

--- app/src/main/java/com/udhay/helfy/data/CasesRepository.kt
+++ app/src/main/java/com/udhay/helfy/data/CasesRepository.kt
@@ -1,16 +1,23 @@
 package com.udhay.helfy.data
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import com.udhay.helfy.data.model.AppUpdateModel
 import com.udhay.helfy.data.model.CountryModel
 import com.udhay.helfy.data.model.StateModel
 import com.udhay.helfy.data.model.WorldModel
 import com.udhay.helfy.util.testWorldJson
+import kotlinx.coroutines.suspendCancellableCoroutine
 import retrofit2.Response
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 class CasesRepository(
     private val service: CasesService
 ) {
+    private val db = Firebase.firestore
+
     suspend fun fetchCountryData(): Response<CountryModel> {
         return service.fetchCountryData()
     }
@@ -27,12 +34,22 @@ class CasesRepository(
     }
 
     suspend fun fetchCurrentVersion(): Response<AppUpdateModel> {
-        return Response.success(
-            200,
-            AppUpdateModel(
-                5,
-                "http://s3.apk4all.com/games/2019/04/Battlelands_Royale_1.6.2_Mod_Apk4all.com.apk"
-            )
-        )
+        var response: Response<AppUpdateModel>
+        return suspendCancellableCoroutine { coroutine ->
+            db.collection("updates")
+                .get()
+                .addOnSuccessListener { result ->
+                    val updateModel = AppUpdateModel(
+                        result.documents[0].getLong("version_code")?.toInt() ?: 1,
+                        result.documents[0].getString("download_url") ?: ""
+                    )
+                    response = Response.success(
+                        200,
+                        updateModel
+                    )
+                    coroutine.resume(response)
+                }
+
+        }
     }
 }
